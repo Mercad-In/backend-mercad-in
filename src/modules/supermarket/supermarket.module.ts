dfe463797a0148b9d953ce40242b535f9af0beed
Marlon <Marlonnlmx@outlook.com>
2023-06-09 00:48:14 -0300

    feat: provider atualizado

--- src/modules/supermarket/supermarket.module.ts
+++ src/modules/supermarket/supermarket.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SupermarketService } from './supermarket.service';
 import { SupermarketController } from './supermarket.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { SupermarketRepository } from './repositories/supermarket.repository';
-import { PrismaSupermarketRepository } from './repositories/implementations/prisma.supermarket.repository';
 
 @Module({
   controllers: [SupermarketController],
-  providers: [
-    SupermarketService,
-    PrismaService,
-    PrismaSupermarketRepository,
-    {
-      provide: SupermarketRepository,
-      useClass: PrismaSupermarketRepository,
-    },
-  ],
+  providers: [SupermarketService, PrismaService, SupermarketRepository],
   exports: [PrismaService, SupermarketService, SupermarketRepository],
 })
 export class SupermarketModule {}
