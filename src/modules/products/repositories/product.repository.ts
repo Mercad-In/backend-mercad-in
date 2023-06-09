98b4033d94fb921c80ad0a1507fcdb366437cde8
Marlon <Marlonnlmx@outlook.com>
2023-06-09 00:46:26 -0300

    feat: mudando escopo da classe abstrata para uma classe repositório

--- src/modules/products/repositories/product.repository.ts
+++ src/modules/products/repositories/product.repository.ts
@@ -1,8 +1,10 @@
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { CreateProductDto } from '../dtos/create-product.dto';
 import { UpdateProductDto } from '../dtos/update-product.dto';
 import { Product } from '../entities/product.entity';
 
+@Injectable()
 export class ProductRepository {
   constructor(private readonly prisma: PrismaService) {}
 
--- src/modules/supermarket/repositories/supermarket.repository.ts
+++ src/modules/supermarket/repositories/supermarket.repository.ts
@@ -1,12 +1,68 @@
+import { PrismaService } from '../../../prisma/prisma.service';
 import { CreateSupermarketDto } from '../dtos/create-supermarket.dto';
 import { UpdateSupermarketDto } from '../dtos/update-supermarket.dto';
-import { SupermarketEntity } from '../entities/supermarket.entity';
+import { Supermarket } from '../entities/supermarket.entity';
+import { Injectable } from '@nestjs/common';
 
-export abstract class SupermarketRepository {
-  abstract findById(id: string): Promise<SupermarketEntity | null>;
-  abstract findAll(): Promise<SupermarketEntity[]>;
-  abstract findByEmail(email: string): Promise<SupermarketEntity | null>;
-  abstract create(data: CreateSupermarketDto): Promise<void>;
-  abstract update(id: string, data: UpdateSupermarketDto): Promise<void>;
-  abstract delete(id: string): Promise<void>;
+@Injectable()
+export class SupermarketRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<Supermarket[] | null> {
+    const allregisters = await this.prisma.supermarket.findMany();
+
+    if (!allregisters) {
+      return null;
+    }
+
+    return allregisters;
+  }
+
+  async findById(id: string): Promise<Supermarket | null> {
+    const supermarket = await this.prisma.supermarket.findUnique({
+      where: { id },
+    });
+    if (!supermarket) {
+      return null;
+    }
+    return supermarket;
+  }
+
+  async findByEmail(email: string): Promise<Supermarket | null> {
+    const supermarket = await this.prisma.supermarket.findFirst({
+      where: { email },
+    });
+    if (!supermarket) {
+      return null;
+    }
+    return supermarket;
+  }
+
+  async create(data: CreateSupermarketDto): Promise<void> {
+    await this.prisma.supermarket.create({ data });
+  }
+
+  async update(id: string, data: UpdateSupermarketDto): Promise<void> {
+    await this.prisma.supermarket.update({
+      where: { id },
+      data: {
+        name: data.name,
+        email: data.email,
+        cnpj: data.cnpj,
+        password: data.password,
+      },
+    });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prisma.supermarket.delete({ where: { id } });
+  }
+
+  async deleteByEmail(email: string): Promise<void> {
+    await this.prisma.supermarket.deleteMany({
+      where: {
+        email,
+      },
+    });
+  }
 }
